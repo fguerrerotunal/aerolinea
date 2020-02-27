fc318ac132b34d752b47af8bf62f8b8c1340167b
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-02-26 21:38:14 -0500

    Update Admin.java

--- src/gestorAplicacion/Admin.java
+++ src/gestorAplicacion/Admin.java
@@ -53,22 +53,5 @@ public class Admin {
 		}
 		return posicion;
 	}
-//	public static void main(String[] args) {
-		
-		
-//		Scanner sc = new Scanner(System.in);
-		//		Cliente cliente1 = new Cliente(1002899765, 123, "federico", "cra35No46-60", "fredde@gmail.com", 1234);
-		//		Empleado empleado1 = new Empleado(1002000123, 321 ,"pirobo", "carlosE", "maricatu@gmail.com", "gestordevuelo");
-		//while(true) {
-		//String opccion = sc.next();
-		//switch (opccion) {
-		//	case "invitado":
-		//		System.out.println("pantalla invitado");
-		//		break;
-		//	case "usuario":
-		//		
-		//}
-		//}
-//	}
 
 }
