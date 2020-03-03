464e270269e1f660ebf0f5651585750429a68d53
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-02 20:33:09 -0500

    reservas

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -10,12 +10,15 @@ public class Reader {
 	FileReader Clientes;
 	FileReader Empleados;
 	FileReader Vuelos;
+	FileReader Reservas;
 	public void leer() throws FileNotFoundException{
 		Aeropuertos=new FileReader("\\Temp\\GitHub\\aerolinea\\src\\Basededatos\\Aeropuerto.txt");
 		Clientes=new FileReader("\\Temp\\GitHub\\aerolinea\\src\\Basededatos\\Clientes.txt");
 		Empleados=new FileReader("\\Temp\\GitHub\\aerolinea\\src\\Basededatos\\Empleados.txt");
 		Vuelos=new FileReader("\\Temp\\GitHub\\aerolinea\\src\\Basededatos\\Vuelos.txt");
-		for (int j,j<Admin.destinos.size(),i++) {
+		Reservas=new FileReader("\\Temp\\GitHub\\aerolinea\\src\\Basededatos\\Reservas.txt");
+		for (int i=0;i<Admin.destinos.size();i++) {
+			
 			
 		}
 		
