e246e4e6208d1679a5b2a233de57afddba10d5c6
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-02 19:16:34 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -2,6 +2,12 @@ package Basededatos;
 import java.io.*;
 
 public class Reader {
+	FileReader Aeropuertos;
+	FileReader Clientes;
+	FileReader Empleados;
+	FileReader Vuelos;
+	public void leer() {
+		
+	}
 	
-
 }
