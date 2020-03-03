0e34e1b7cedb5b8fb4bcdc43b4c77a363cdb6f20
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-02 19:50:43 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -1,5 +1,8 @@
 package Basededatos;
 import java.io.*;
+
+import javax.imageio.IIOException;
+
 import gestorAplicacion.*;
 
 public class Reader {
@@ -7,11 +10,11 @@ public class Reader {
 	FileReader Clientes;
 	FileReader Empleados;
 	FileReader Vuelos;
-	public void leer() {
-		Aeropuertos=new FileReader();
-		Clientes=new FileReader();
-		Empleados=new FileReader();
-		Vuelos=new FileReader();
+	public void leer() throws FileNotFoundException {
+		Aeropuertos=new FileReader("https://github.com/fguerrerotunal/aerolinea/blob/master/src/Basededatos/Aeropuertos.txt");
+		Clientes=new FileReader("https://github.com/fguerrerotunal/aerolinea/blob/master/src/Basededatos/Clientes.txt");
+		Empleados=new FileReader("https://github.com/fguerrerotunal/aerolinea/blob/master/src/Basededatos/Empleados.txt");
+		Vuelos=new FileReader("https://github.com/fguerrerotunal/aerolinea/blob/master/src/Basededatos/Vuelos.txt");
 	}
 	
 }
