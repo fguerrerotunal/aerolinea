bd8c2796534e68cb0c42f13b1ed6eaea5597d5a9
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-03 20:35:16 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -1,10 +1,8 @@
 package Basededatos;
 import java.io.*;
-import java.util.Vector;
-import javax.imageio.IIOException;
 import gestorAplicacion.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 public class Reader {
+	ObjectInputStream entradas;
+	entradas= new ObjectInputStream(new FileInputStream("C:\\\\Users\\\\crist\\\\Desktop\\\\aerolinea\\\\src\\\\Basededatos\\\\Aeropuertos.txt"));
+	
 }
