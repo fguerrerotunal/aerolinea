1b65e4e4e4c88199b64990c42b9a10b5b0c8e048
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-03 22:03:01 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -20,7 +20,7 @@ public class Reader {
 				Objeto=entradas.readObject();
 			}
 			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream("...Empleados.txt"));
+			entradas = new ObjectInputStream(new FileInputStream("Empleados.txt"));
 			Objeto=entradas.readObject();
 			while (Objeto!=null) {
 				Admin.empleados.add((Empleado) Objeto);
