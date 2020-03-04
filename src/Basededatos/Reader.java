b152febfed7b307a6073988906b70db85836240f
fguerrerotunal <fredderico16@gmail.com>
2020-03-03 23:37:40 -0500

    correccion

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -11,30 +11,46 @@ public class Reader {
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
 
 			Objeto=entradas.readObject();
-			while (Objeto!=null) {
-				Admin.destinos.add((Aeropuerto) Objeto);
-				Objeto=entradas.readObject();
+			try{
+				while (Objeto!=null) {
+					Admin.destinos.add((Aeropuerto) Objeto);
+					Objeto=entradas.readObject();
+				}
+			}catch(java.io.EOFException e){
+				
 			}
 			entradas.close();
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Clientes.txt"));
 			Objeto=entradas.readObject();
-			while (Objeto!=null) {
+			try{
+				while (Objeto!=null) {
 				Admin.clientes.add((Cliente) Objeto);
 				Objeto=entradas.readObject();
+				}
+			}catch(java.io.EOFException e){
+			
 			}
 			entradas.close();
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Empleados.txt"));
 			Objeto=entradas.readObject();
-			while (Objeto!=null) {
-				Admin.empleados.add((Empleado) Objeto);
-				Objeto=entradas.readObject();
+			try{
+				while (Objeto!=null) {
+					Admin.empleados.add((Empleado) Objeto);
+					Objeto=entradas.readObject();
+				}
+			}catch(java.io.EOFException e){
+				
 			}
 			entradas.close();
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Vuelos.txt"));
 			Objeto=entradas.readObject();
-			while (Objeto!=null) {
-				Empleado.vuelos.add((Vuelo) Objeto);
-				Objeto=entradas.readObject();
+			try{
+				while (Objeto!=null) {
+					Empleado.vuelos.add((Vuelo) Objeto);
+					Objeto=entradas.readObject();
+				}
+			}catch(java.io.EOFException e){
+				
 			}
 			entradas.close();
 		}catch(Exception e) {
