953d89faa5150ecd09927d501afed2db2231b879
fguerrerotunal <fredderico16@gmail.com>
2020-03-03 21:00:15 -0500

    error

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -2,8 +2,12 @@ package Basededatos;
 import java.io.*;
 import gestorAplicacion.*;
 public class Reader {
-	FileInputStream Aeropuertos=new FileInputStream("C:\\Users\\crist\\Desktop\\aerolinea\\src\\Basededatos\\Aeropuertos.txt");
-	ObjectInputStream entradas=new ObjectInputStream(Aeropuertos);
-	
+	public static void Leer() throws Exception{
+		try {
+			FileInputStream Aeropuertos=new FileInputStream("C:\\Users\\crist\\Desktop\\aerolinea\\src\\Basededatos\\Aeropuertos.txt");
+			ObjectInputStream entradas=new ObjectInputStream(Aeropuertos);
+		}catch(Exception e) {
+			System.out.println("a");
+		}
+	}
 }
-;
