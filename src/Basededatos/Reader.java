2ec0487ada78cafd6e2d84b446e763b4f3f1b01b
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-03 22:09:39 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -3,11 +3,11 @@ import java.io.*;
 import gestorAplicacion.*;
 public class Reader {
 	private static ObjectInputStream entradas;
-	static File Aeropuertos = new File("");
+	static File fichero = new File("");
 	public static void Leer(){
 		try {
 
-			entradas = new ObjectInputStream(new FileInputStream(Aeropuertos.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
+			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
 
 			Object Objeto=entradas.readObject();
 			while (Objeto!=null) {
@@ -15,21 +15,21 @@ public class Reader {
 				Objeto=entradas.readObject();
 			}
 			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream("...Clientes.txt"));
+			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Clientes.txt"));
 			Objeto=entradas.readObject();
 			while (Objeto!=null) {
 				Admin.clientes.add((Cliente) Objeto);
 				Objeto=entradas.readObject();
 			}
 			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream("Empleados.txt"));
+			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Empleados.txt"));
 			Objeto=entradas.readObject();
 			while (Objeto!=null) {
 				Admin.empleados.add((Empleado) Objeto);
 				Objeto=entradas.readObject();
 			}
 			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream("...Vuelos.txt"));
+			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Vuelos.txt"));
 			Objeto=entradas.readObject();
 			while (Objeto!=null) {
 				Empleado.vuelos.add((Vuelo) Objeto);
