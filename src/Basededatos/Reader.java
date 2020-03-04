40cbe2cf77b1fca17ac638a091b72935fe5434a4
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-03 20:40:59 -0500

    Update Reader.java

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -2,7 +2,6 @@ package Basededatos;
 import java.io.*;
 import gestorAplicacion.*;
 public class Reader {
-	ObjectInputStream entradas;
-	entradas= new ObjectInputStream(new FileInputStream("C:\\\\Users\\\\crist\\\\Desktop\\\\aerolinea\\\\src\\\\Basededatos\\\\Aeropuertos.txt"));
+	ObjectInputStream entradas=new ObjectInputStream(new FileInputStream(fichero));
 	
 }
