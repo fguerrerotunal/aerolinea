7faa1afc7202ab57b59913b6a116afaf647551e1
fguerrerotunal <fredderico16@gmail.com>
2020-03-05 19:12:11 -0500

    cancelar

--- src/uiMain/menuconsola/CancelarReserva.java
+++ src/uiMain/menuconsola/CancelarReserva.java
@@ -7,8 +7,13 @@ public class CancelarReserva extends OpcionDeMenu{
 	}
 	
 	public void Ejecutar() {
-		System.out.println("¿Que vuelo deseas cancelar?");
-		int vuelo = sc.nextInt();
-		System.out.println(MenuDeConsola.usuarioactual.cancelarReserva(MenuDeConsola.usuarioactual.cartera.get(vuelo)));
+		if(!MenuDeConsola.usuarioactual.cartera.isEmpty()) {
+			System.out.println("¿Que vuelo deseas cancelar?");
+			int vuelo = sc.nextInt();
+			System.out.println(MenuDeConsola.usuarioactual.cancelarReserva(MenuDeConsola.usuarioactual.cartera.get(vuelo)));
+		}else {
+			System.out.println("No cuentas con reservas activas por el momento.");
+		}
+		
 	}
 }
