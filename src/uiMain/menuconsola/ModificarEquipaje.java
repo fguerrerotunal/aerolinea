d72e8c6c52ffd76e8ba9a55588ece79732018845
jhoan3211 <50278559+jhoan3211@users.noreply.github.com>
2020-03-05 21:18:34 -0500

    Update ModificarEquipaje.java

--- src/uiMain/menuconsola/ModificarEquipaje.java
+++ src/uiMain/menuconsola/ModificarEquipaje.java
@@ -10,23 +10,27 @@ public class ModificarEquipaje  extends OpcionDeMenu{
 	}
 	
 	public void Ejecutar() {
-		System.out.println("Vuelo a cambiar equipaje: ");
-		int posicion = sc.nextInt();
-		System.out.println("Nuevo peso del equipaje: ");
-		int Npeso = sc.nextInt();
-		
-		Reserva reserva = MenuDeConsola.usuarioactual.cartera.get(posicion);
-		System.out.println("ESTA ACCION TIENE UN COSTO DE: "+ reserva.getCosto()+"\n¿DESEA CONTINUAR?\nSI=0    NO=1");
-		int continuar = sc.nextInt();
-		if(continuar==0) {
-			MenuDeConsola.usuarioactual.cartera.get(posicion).setEquipaje(Npeso);
-			System.out.println("¿Que medio de pago desea usar?\nEfectivo = 0\tMillas = 1");
-			int medio = sc.nextInt();
-			System.out.println(MenuDeConsola.usuarioactual.Pago(medio, reserva));
-		}else {
-			System.out.println("OPERACON CANCELADA");
+			System.out.println("Vuelo a cambiar equipaje: ");
+			int posicion = sc.nextInt();
+			if(MenuDeConsola.usuarioactual.cartera.get(posicion).vuelo.estado.equals("Venta")){
+			System.out.println("Nuevo peso del equipaje: ");
+			int Npeso = sc.nextInt();
+			
+			Reserva reserva = MenuDeConsola.usuarioactual.cartera.get(posicion);
+			System.out.println("ESTA ACCION TIENE UN COSTO DE: "+ reserva.getCosto()+"\n¿DESEA CONTINUAR?\nSI=0    NO=1");
+			int continuar = sc.nextInt();
+			if(continuar==0) {
+				MenuDeConsola.usuarioactual.cartera.get(posicion).setEquipaje(Npeso);
+				System.out.println("¿Que medio de pago desea usar?\nEfectivo = 0\tMillas = 1");
+				int medio = sc.nextInt();
+				System.out.println(MenuDeConsola.usuarioactual.Pago(medio, reserva));
+			}else {
+				System.out.println("OPERACON CANCELADA");
+			}
 		}
-		
+		else {
+			System.out.println("Solo puedes modificarlo en estado de venta");
+		}
 		
 	}
 }
