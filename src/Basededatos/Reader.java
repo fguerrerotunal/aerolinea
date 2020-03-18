b1abc17666dd625573a2a8c9e4d3bcad0a818ae5
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-17 21:03:03 -0500

    aun no funciona

--- src/Basededatos/Reader.java
+++ src/Basededatos/Reader.java
@@ -1,64 +1,54 @@
 package Basededatos;
 import java.io.*;
-import com.google.*;
+import java.util.Vector;
+import java.lang.reflect.Type;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import gestorAplicacion.AtencionAlCliente.*;
 import gestorAplicacion.Master.*;
+import java.io.*;
 public class Reader {
-	private static ObjectInputStream entradas;
+	static Gson archivo=new Gson();
 	static File fichero = new File("");
-	static Object Objeto;
 	public static void Leer(){
 		try {
-
-			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
-
-			Objeto=entradas.readObject();
-			try{
-				while (Objeto!=null) {
-					Admin.destinos.add((Aeropuerto) Objeto);
-					Objeto=entradas.readObject();
-				}
-			}catch(java.io.EOFException e){
-				
+	       JsonElement Aeropuertos=lector.parse(new FileReader(fichero.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
+	       
+	        Type listType = new TypeToken<Vector<Aeropuerto>>(){}.getType();
+			Admin.destinos= archivo.fromJson(Aeropuertos,listType);
+		}catch(Exception e){
+			System.out.println("ERROR al entrar los datos");
+			System.out.println(e);
 			}
-			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Clientes.txt"));
-			Objeto=entradas.readObject();
-			try{
-				while (Objeto!=null) {
-				Admin.clientes.add((Cliente) Objeto);
-				Objeto=entradas.readObject();
-				}
-			}catch(java.io.EOFException e){
+		JsonParser parser = new JsonParser();
+		try{
+			Object obj = parser.parse(new FileReader(fichero.getAbsolutePath()+"\\src\\Basededatos\\Aeropuertos.txt"));
+			 
+			JsonObject jsonObject = (JsonObject) obj;
+	 
+			String nombre = (String) jsonObject.get("nombre");
+			System.out.println("nombre:"+nombre);
+	 
+			long edad = (Long) jsonObject.get("edad");
+			System.out.println("edad:"+edad);
+	 
+			// recorrer arreglo
+			JSONArray leng= (JSONArray) jsonObject.get("lenguajes_favoritos");
+			System.out.println("lenguajes_favoritos:");
+			Iterator iterator =leng.iterator();
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
 			
-			}
-			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Empleados.txt"));
-			Objeto=entradas.readObject();
-			try{
-				while (Objeto!=null) {
-					Admin.empleados.add((Empleado) Objeto);
-					Objeto=entradas.readObject();
-				}
-			}catch(java.io.EOFException e){
-				
-			}
-			entradas.close();
-			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath()+"\\src\\Basededatos\\Vuelos.txt"));
-			Objeto=entradas.readObject();
-			try{
-				while (Objeto!=null) {
-					Empleado.vuelos.add((Vuelo) Objeto);
-					Objeto=entradas.readObject();
-				}
-			}catch(java.io.EOFException e){
-				
-			}
-			entradas.close();
-		}catch(Exception e) {
-
-			System.out.println(e.toString());
-
+		}catch(Exception ex){
+			System.err.println("Error: "+ex.toString());
+		}finally{
+			
 		}
-	}
+
+	}  
+
 }
