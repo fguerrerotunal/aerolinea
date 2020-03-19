74b207d4b517e6018ac91436d8918e3669482879
fguerrerotunal <fredderico16@gmail.com>
2020-03-19 18:28:32 -0500

    formularios genericos...falta

---
+++ src/InterfaceGrafica/FieldPanel.java
@@ -0,0 +1,34 @@
+package InterfaceGrafica;
+
+import java.lang.reflect.Array;
+
+public class FieldPanel extends Pane {
+	
+	String tituloCriterios;
+	String[] criterios;
+	String tituloValores;
+	String[]  valores;
+	boolean[] habilitado;
+	
+	public FieldPanel(String tituloCriterios, String[] criterios, String tituloValores, String[] valores,
+			boolean[] habilitado) {
+		super();
+		this.tituloCriterios = tituloCriterios;
+		this.criterios = criterios;
+		this.tituloValores = tituloValores;
+		this.valores = valores;
+		this.habilitado = habilitado;
+	}
+	
+	public String getValue(String criterio) {
+		String valor;
+		for(int i = 0; i< this.criterios.length;i++) {
+			if(this.criterios[i].equals(criterio)) {
+				valor = this.valores[i]; 
+			}
+		}
+		return valor;
+	}
+	
+	
+}
