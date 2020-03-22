015d022e9cebf32cc18f8d12a774f50345978591
fguerrerotunal <fredderico16@gmail.com>
2020-03-21 20:08:38 -0500

    jhb

--- src/Utilidades/ErrorAplicacion.java
+++ src/Utilidades/ErrorAplicacion.java
@@ -2,6 +2,6 @@ package Utilidades;
 
 public class ErrorAplicacion extends Exception{
 	ErrorAplicacion(String error){
-		super("Manejo de errores de la aplicacion"+error);
+		super("Manejo de errores de la aplicacion: "+error);
 	}
 }
