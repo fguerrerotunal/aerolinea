ea00bcee57adfb6028c896b962d4ecdd0f4a7c24
jhoan3211 <50278559+jhoan3211@users.noreply.github.com>
2020-03-24 20:42:30 -0500

    este fue 2

--- src/InterfaceGrafica/VInicio.java
+++ src/InterfaceGrafica/VInicio.java
@@ -456,7 +456,7 @@ public class VInicio extends Application {
 					} 
 				}));
 				GridPane V = new GridPane();
-				
+				P.getItems().setAll(datos());
 				ComboBox<String> Si = new ComboBox<String>();
 	    		Si.getItems().addAll(
 	    			    "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"
