e527bacaf771f7bbcd39aeab61596c70ea8359b0
jhoan3211 <50278559+jhoan3211@users.noreply.github.com>
2020-03-24 20:46:22 -0500

    Update VInicio.java

--- src/InterfaceGrafica/VInicio.java
+++ src/InterfaceGrafica/VInicio.java
@@ -65,7 +65,7 @@ public class VInicio extends Application {
 				Admin.empleados.get(0).ActualizarVuelos();
 			}
 		};
-		timer.schedule(estadoVuelos, 2000,2000);//cada 1 min
+		timer.schedule(estadoVuelos, 20000,20000);//cada 1 min
 		launch(args);
 	}
 	
