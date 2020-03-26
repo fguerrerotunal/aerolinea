82c1d5b7444fee075fb567d5f2e8171f07c3c381
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-26 17:19:45 -0500

    d

--- src/gestorAplicacion/AtencionAlCliente/CuentaMillas.java
+++ src/gestorAplicacion/AtencionAlCliente/CuentaMillas.java
@@ -1,6 +1,7 @@
 package gestorAplicacion.AtencionAlCliente;
 import java.util.*;
 import java.io.*;
+//Cada cliente tiene un objeto cuentamillas,este se encarga de guardar el ciente, y las millas que va recolectando el usuario por sus viajes.
 public class CuentaMillas{
 //Atributos de los objetos de tipo clase cuentamillas
 	private static int cantidadCuentas;
