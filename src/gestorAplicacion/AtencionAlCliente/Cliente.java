93643a0ab62cb5ce40611226687055d9359b8c6b
Cjara26 <53546910+Cjara26@users.noreply.github.com>
2020-03-25 21:24:44 -0500

    Update Cliente.java

--- src/gestorAplicacion/AtencionAlCliente/Cliente.java
+++ src/gestorAplicacion/AtencionAlCliente/Cliente.java
@@ -21,7 +21,6 @@ public class Cliente extends Persona{
 	//constructor de cliente
 	public Cliente(int identificacion, int cuentabancaria, String nombre, String direccion, String correo, int pasaporte){
 		super(identificacion, cuentabancaria, nombre, direccion, correo);
-		numero_de_clientes ++;
 		this.setCuentamillas(new CuentaMillas(this,identificacion));
 		this.setPasaporte(pasaporte);
 	}
